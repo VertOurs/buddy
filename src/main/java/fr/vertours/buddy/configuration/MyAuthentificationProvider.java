b0cb822c61f547f304c1ea0e6d99091a71746eb6
Ours <vertours.dev@gmail.com>
2021-12-10 15:24:33 +0100

    Exemple Armel Config-sécu
    mise en place du package Config

---
+++ src/main/java/fr/vertours/buddy/configuration/MyAuthentificationProvider.java
@@ -0,0 +1,23 @@
+package fr.vertours.buddy.configuration;
+
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+
+@Component
+public class MyAuthentificationProvider implements AuthenticationProvider {
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        return new UsernamePasswordAuthenticationToken("admin", "admin", new ArrayList<>());
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return authentication.equals(UsernamePasswordAuthenticationToken.class);
+    }
+}
