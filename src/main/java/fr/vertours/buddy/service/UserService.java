c91eca7688fbae10f2563527cd960c223934927a
Ours <vertours.dev@gmail.com>
2021-12-22 15:47:17 +0100

    essai de service controlleur avec postman

--- src/main/java/fr/vertours/buddy/service/UserService.java
+++ src/main/java/fr/vertours/buddy/service/UserService.java
@@ -2,15 +2,17 @@ package fr.vertours.buddy.service;
 
 import fr.vertours.buddy.model.User;
 import fr.vertours.buddy.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
 
-    @Autowired
+    final
     UserRepository userRepository;
 
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     public void saveUser(User user) {
         userRepository.save(user);
@@ -18,5 +20,12 @@ public class UserService {
 
     public User findByEmail(String email) {
         return userRepository.findByMail(email);
+    }
+
+    public void deleteFriendWithUserId(Long friendId, Long userId) {
+        User mainUser = userRepository.getById(userId);
+        //intellij proposition for replace my loop
+        mainUser.getMyFriendList().removeIf(u -> u.getId() == friendId);
+        userRepository.save(mainUser);
     }
 }
